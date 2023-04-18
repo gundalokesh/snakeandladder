89d320a6bcd38d2fe90af64c08fbb651ad242fef
gundalokesh <gundalokesh2000@gmail.com>
2023-04-18 19:42:13 +0530

    "add img and dies works baes on dies number"

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 function player1(){
     let x = Math.floor(Math.random() * 6) + 1 ;
 document.querySelector(".player1").innerHTML = x;
+if(x <= 1){
+  document.querySelector(".id1").style.left =  `${(x)* 20}px`;
+
+  document.querySelector(".id1").style.zIndex = "1000";
+
+}
+
+else{
+  document.querySelector(".id1").style.left =  `${(x - 1)* 79.86 }px`;
+}
+
+ 
+
+
+
+
+
+
+
 }
 function player2(){
     let x = Math.floor(Math.random() * 6) + 1 ;
 document.querySelector(".player2").innerHTML = x;
+if(x <= 1){
+  document.querySelector(".id2").style.left =  `${(x)* 20}px`;
+
+  document.querySelector(".id2").style.zIndex = "1000";
+
+}
+else{
+  document.querySelector(".id2").style.left =  `${(x - 1)* 79.86 }px`;
+}
 }
 
 const container = document.querySelector(".grid-container");
@@ -12,16 +40,23 @@ const container = document.querySelector(".grid-container");
 
 
 
-for (let i = 100; i > 0; i--) {
+for (let i = 0; i < 100; i++) {
   const div = document.createElement("div");
-  const number = document.createTextNode((i + 0));
  
-  div.className="grid-item";
+ 
+  div.className="grid-item-"+ i;
   container.appendChild(div);
-  div.appendChild(number);
+
   
 
 }
+
+
+
+
+
+
+
 
 
 
